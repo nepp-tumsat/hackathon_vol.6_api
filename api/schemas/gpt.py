f1e01c52da2b3fa03a01d8a6b8d16f5c246a3f78
Kentaro Matsubara <mmmmmmmmm.oh2point@gmail.com>
2023-07-15 22:09:02 +0900

    変数変更

--- api/schemas/gpt.py
+++ api/schemas/gpt.py
@@ -2,8 +2,8 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-class Task(BaseModel):
-    mane: Optional[str] = Field(None, example="松原健太郎")
+class Info(BaseModel):
+    name: Optional[str] = Field(None, example="松原健太郎")
     job: Optional[str] = Field(None, example="大学生")
-    hoby: Optional[str] = Field(None, example="")
-    id: int
+    hobby: Optional[str] = Field(None, example="アニメ鑑賞、テニス、マラソン")
+    money: Optional[str] = Field(None, example="40000")

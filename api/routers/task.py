86e63f2d97e83da05dedfda669f803340864475f
{Ryu} <{r216af@icloud.com}>
2023-07-15 17:17:16 +0900

    不要なデコレータの削除

--- api/routers/task.py
+++ api/routers/task.py
@@ -7,9 +7,6 @@ import api.schemas.task as task_schema
 router = APIRouter()
 
 
-#@router.get("/tasks")
-#async def list_tasks():
-#    pass
 @router.get("/tasks", response_model=List[task_schema.Task])
 async def list_tasks():
     return [task_schema.Task(id=1, title="1つ目のTODOタスク", limit="YYYY-MM-DDThh:mm:ss.sssZ")]

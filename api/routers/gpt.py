18ab82ee50161e34a1ef513e2ee09a1eada158d9
Kentaro Matsubara <mmmmmmmmm.oh2point@gmail.com>
2023-07-15 15:38:58 +0900

    openaiのテスト

--- api/routers/gpt.py
+++ api/routers/gpt.py
@@ -1,12 +1,19 @@
 from fastapi import APIRouter
-
+import os
 import openai
 
 router = APIRouter()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @router.get("/gpt")
 async def gpt_advice():
-    pass
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": "お勧めの気分転換は？"},
+    ],
+    )
+    return response.choices[0]["message"]["content"].strip()
 
 
 @router.post("/gpt")

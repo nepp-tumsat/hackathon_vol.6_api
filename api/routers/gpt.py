30e82322853df4db0790ec4be0ec28e9054cf512
Kentaro Matsubara <86394918+shirasu-48@users.noreply.github.com>
2023-07-15 21:39:22 +0900

    GPTのAPI仮完成

--- api/routers/gpt.py
+++ api/routers/gpt.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 import openai
+import os
 #import api.schemas.gpt as gpt_schema
 from typing import Optional
 from pydantic import BaseModel, Field
@@ -8,16 +9,16 @@ class Info(BaseModel):
     name: Optional[str] = Field(None, example="松原健太郎")
     job: Optional[str] = Field(None, example="大学生")
     hobby: Optional[str] = Field(None, example="アニメ鑑賞、テニス、マラソン")
-    money: int
+    money: Optional[str] = Field(None, example="40000")
 
 router = APIRouter()
-openai.api_key = ""
-job='学生'
-hobby='将棋、囲碁、テニス'
-money="20000"
+openai.api_key = "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"
 
 @router.get("/gpt")
 async def gpt_advice():
+    job=os.environ.get('USERSJOB')
+    hobby=os.environ.get('USERSHOBBY')
+    money=os.environ.get('USERSMONEY')
 
     qestion="私の職業は"+job+"です。最近の趣味は"+hobby+"です。最近の趣味や職業に関連して"+money+"円程度でできる趣味を教えてください。"
     response = openai.ChatCompletion.create(
@@ -32,4 +33,8 @@ async def gpt_advice():
 
 @router.post("/gpt")
 async def add_info(info: Info):
+    os.environ['USERSNAME']=info.name
+    os.environ['USERSJOB']=info.job
+    os.environ['USERSHOBBY']=info.hobby
+    os.environ['USERSMONEY']=info.money
     return {"res": "ok" , "name": info.name , "job": info.job ,"hobby": info.hobby ,"money":info.money}

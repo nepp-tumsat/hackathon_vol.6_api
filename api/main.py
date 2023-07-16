dae3d82f49b47d9fde02946fba3681881a832505
Kentaro Matsubara <86394918+shirasu-48@users.noreply.github.com>
2023-07-16 12:00:54 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 
 from api.routers import task, done ,gpt
 
+from starlette.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
+
+
 app.include_router(task.router)
 app.include_router(done.router)
 app.include_router(gpt.router)
